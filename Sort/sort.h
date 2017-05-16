edbf044040987b581ac5845954bf22c1edf0041d
msamatar0 <mohamedsamatar100@gmail.com>
2017-05-16 15:54:25 -0700

    Update sort.h

--- Sort/sort.h
+++ Sort/sort.h
@@ -11,7 +11,7 @@ void init(int *order, int *rev, int *random, size_t size);
 template<class type, class comparator> void heapSort(type *arr, size_t size, comparator comp);
 template<class type, class comparator> void mergeSort(type *arr, size_t l, size_t r, comparator comp);
 template<class type, class comparator> void merge(type *arr, size_t left, size_t mid, size_t right, comparator comp);
-template<class type, class comparator> void quickSort(type *arr, size_t left, size_t right, int ptype, comparator comp);
+template<class type, class comparator> void quickSort(type *arr, size_t left, size_t right, comparator comp);
 template<class type, class comparator> void bucketSort(type *arr, size_t size, comparator comp);
 template<class type, class comparator> void radixSort(type *arr, size_t size, comparator comp);
 void print(int *arr);
